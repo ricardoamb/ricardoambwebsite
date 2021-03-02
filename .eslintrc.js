e336f34fd1785806476ae449d21692d00e6b6201
ricardoamb <ricardoamb.dev@gmail.com>
2021-03-02 16:28:53 -0300

    Cleanup Starter Stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    extends: ['plugin:react/recommended', 'airbnb'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        'react/jsx-indent': ['error', 4],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'react/jsx-props-no-spreading': 'off',
+        'react/prop-types': 'off',
+        'react/jsx-indent-props': [2, 4],
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    },
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Ricardo Amb`,
+        description: `Designer por formação, desenvolvedor por paixão. Acho que essa frase é a que mais resume a minha essência profissional. Ao longo dos tempos tive a oportunidade de trabalhar em mais de 100 projetos no Brasil e no mundo. A grande maioria destes projetos entregues de forma 100% online. Isso me faz focar mais energia no projeto e permite atender clientes de qualquer lugar do mundo.`,
+        descriptionEn: `Designer by formation, developer by passion. I think this is the better way to define my professional essence. Over the years I had the opportunity to wotk with more than 100 projects on Brazil and in the world. The vast majority of these projects are delivered 100% online. This makes me focus more energy on the project and allows me to serve customers from anywhere in the world.`,
+        descriptionEs: `Diseñador por formación, desarrollador por pasión. Creo que esta frase es la que más resume mi esencia profesional. A lo largo de los años tuve la oportunidad de trabajar en más de 100 proyectos en Brasil y en todo el mundo. La gran mayoría de estos proyectos se entregan 100% online. Esto me hace concentrar más energía en el proyecto y me permite atender a clientes de cualquier parte del mundo.`,
+        author: `@ricardoambdev`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#161D26`,
+                theme_color: `#161D26`,
+                display: `minimal-ui`,
+                icon: `src/images/my-icon.png`,
+            },
+        },
+        `gatsby-plugin-gatsby-cloud`,
+        `gatsby-plugin-offline`,
+    ],
+};
